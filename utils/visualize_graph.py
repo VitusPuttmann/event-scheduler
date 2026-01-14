0389bb4be379ddb4600d997394220956421e457d
VitusPuttmann <vitus.puttmann@outlook.com>
2026-01-14 23:34:58 +0100

    chore: update graph visualization

--- utils/visualize_graph.py
+++ utils/visualize_graph.py
@@ -5,7 +5,7 @@ Visualize the complete LangGraph graph.
 from IPython.display import Image, display
 from langchain_core.runnables.graph import MermaidDrawMethod
 
-from my_graph.agent import graph
+from scheduler_graph.agent import graph
 
 
 def show_graph() -> None:
